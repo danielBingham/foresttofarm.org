42e65ff978766c5eb8c85482848f9223757f10a5
Daniel Bingham <dbingham@theroadgoeson.com>
2015-01-18 09:18:29 -0500

    Initial plant info box.

---
+++ app/views/display-box.blade.php
@@ -0,0 +1,19 @@
+<div id="display-box-template" class="display-box" >
+	<a class="box-link" href="/"></a>
+	<div class="title-box">
+		<a href="">
+		</a>
+	</div>
+	<div class="left">
+		<p class="left-text"></p>
+		<span class="left-description"></span>
+	</div>
+	<div class="center">
+		<p class="center-text"></p>
+		<span class="center-description"></span>
+	</div>
+	<div class="right">
+		<p class="right-text"></p>
+		<span class="right-description"></span>
+	</div>
+</div>
