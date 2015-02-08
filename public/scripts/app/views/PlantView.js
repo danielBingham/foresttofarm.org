dd2326c077dfba179e38973d7ce312c6928662e9
Daniel Bingham <dbingham@theroadgoeson.com>
2015-02-08 11:00:04 -0500

    Updating PlantView to use Mustache template.

--- public/scripts/app/views/PlantView.js
+++ public/scripts/app/views/PlantView.js
@@ -4,18 +4,26 @@
 define([
 	'jquery',
 	'underscore',
-	'backbone'],
-function($, _, Backbone) {
+	'backbone',
+	'mustache',
+	'text!app/templates/plant-box-template.html'],
+function($, _, Backbone,Mustache,template) {
 	return Backbone.View.extend({
 		tagName: 'li',	
 		className: 'display-box',
 
 		render: function() {
-			var display_box = $("#display-box-template").clone();
-			display_box.attr('id', 'plant-' + this.model.get('id'));
-			display_box.find('.title').html(this.model.get('genus') + ' ' + this.model.get('species'));
-			display_box.find('.sub-title').html(this.model.get('common_names')[0].name);
-			this.$el = display_box;
+			var display_box = Mustache.render(
+				template,
+				{
+					id: this.model.get('id'),
+					genus: this.model.get('genus'),
+					species: this.model.get('species'),
+					common_name: this.model.get('common_names')[0].name
+				}
+			);
+
+			this.$el = $.parseHTML(display_box);
 			return this;
 		}
 	});
