aebc6ceb85be2c2c62152d1ba7f18893c9d44a12
Daniel Bingham <dbingham@theroadgoeson.com>
2015-02-08 10:58:30 -0500

    Moving display-box template into Mustache file
    Moving the display-box template in to a separate Mustache template file
    we can load into the js.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,29 +6,6 @@
 	<script src="scripts/library/require.js" data-main="scripts/bootstrap"></script>
 </head>
 <body>
-<div id="templates">
-	<li id="display-box-template" class="display-box" >
-		<a class="box-link" href="/"></a>
-		<div class="title-box">
-			<a href="" class="title">
-			</a><br />
-			<span class="sub-title">
-			</span>
-		</div>
-		<div class="left">
-			<p class="left-text"></p>
-			<span class="left-description"></span>
-		</div>
-		<div class="center">
-			<p class="center-text"></p>
-			<span class="center-description"></span>
-		</div>
-		<div class="right">
-			<p class="right-text"></p>
-			<span class="right-description"></span>
-		</div>
-	</li>
-</div>
 <div id="header">
     <div id="logo"><a href="/">Forest to Farm</a></div>
 	<div class="clear"></div>
