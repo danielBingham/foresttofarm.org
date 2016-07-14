66807eed1c7f4720b360f6d43b87357bf255d918
Daniel Bingham <dbingham@theroadgoeson.com>
2016-07-14 23:54:33 +0000

    Adding an initialize method to the router and commenting.

--- public/scripts/router.js
+++ public/scripts/router.js
@@ -17,11 +17,40 @@ function(
 ) {
 
 return Backbone.Router.extend({
+    /**
+     * What protocol is this website using?
+     */
+    protocol: "http",
+
+    /**
+     * A hash of routes that this router should handle.
+     */
     routes: {
         '': 'index',
         'plant/:id': 'viewPlant'
     },
 
+    /**
+     * Initialize this router to override and handle any links, preventing them
+     * from hitting the backend. 
+     */
+    initialize: function() {
+        Backbone.history.start({ pushState: true });
+        $(document).on('click', 'a:not([data-bypass])', _.bind(function (event) {
+
+            var href = $(this).attr('href');
+            var protocol = this.protocol + '//';
+
+            if (href.slice(protocol.length) !== protocol) {
+              event.preventDefault();
+              this.navigate(href, true);
+            }
+        }, this));    
+    },
+
+    /** 
+     * The site index.  A an auto-paged list of plants available for viewing.
+     */
     index: function() {
         var plants = new Plants();
         var plant_view = new PlantsView({collection:plants});
@@ -29,6 +58,11 @@ return Backbone.Router.extend({
         plants.fetch();
     },
 
+    /**
+     * A detail page to view the details of a single plant.
+     *
+     * @param   {number}    id  The database id of the plant we'd like to view.
+     */
     viewPlant: function(id) {
         alert('Called with ' + id);
 
