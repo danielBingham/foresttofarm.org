36a8710e3f860f24c53595db9471bef3d79cbbd4
Daniel Bingham <dbingham@theroadgoeson.com>
2016-07-31 16:45:25 +0000

    Added create and update logic to the PlantBoxView.
    This also lays out the update / create / render structure that
    will be used for all other views.

--- public/scripts/app/views/PlantBoxView.js
+++ public/scripts/app/views/PlantBoxView.js
@@ -11,18 +11,62 @@ define([
 	'mustache',
 	'text!app/templates/plant-box-template.html'],
 function($, _, Backbone,Mustache,template) {
-	return Backbone.View.extend({
+
+    /**
+     * A view wrapping and managing the list display box for each plant.
+     */
+	var PlantBoxView = Backbone.View.extend({
 		tagName: 'li',	
 		className: 'display-box',
 
-		initialize: function() {
-			this.listenTo(this.collection, 	'sync', this.render);
-		},
+        /**
+         * Has this view been created and attached to the DOM?
+         *
+         * @type    {boolean}
+         */
+        is_attached: false,
 
         /**
-         * Render this plant display box from the template.
+         * Has this element been created?
+         *
+         * @type    {boolean}
          */
-		render: function() {
+        is_created: false,
+
+        /**
+         * Initialize this view.  Bind the change events, create and attach the
+         * DOM element.
+         *
+         * @return  {void}
+         */
+		initialize: function() {
+            this.listenTo(this.model, 'change', _.bind(this.update, this));
+
+            this.create();
+		},
+        
+        /**
+         * Mark whether or not this view has been attached to the DOM.
+         *
+         * @param   {boolean}   attached    If true, this view has been 
+         *      attached to the DOM.  If false, it hasn't been or has been
+         *      removed.
+         *
+         * @return  {this}
+         */
+        setAttached: function(attached) {
+            this.is_attached = attached;
+            return this;
+        },
+
+        /**
+         * Parse this view's mustache template and populate it with data from
+         * the model.
+         *
+         * @return  {JQuery}    A jquery element object with the parsed
+         *  template and model data.
+         */
+        parse: function() {
 			var display_box = Mustache.render(
 				template,
 				{
@@ -33,8 +77,50 @@ function($, _, Backbone,Mustache,template) {
 				}
 			);
 
-            this.setElement($.parseHTML(display_box));
+            return $.parseHTML(display_box);
+        },
+
+        /**
+         * Create this view's element from the mustache template and model.
+         *
+         * @return  {this}
+         */
+        create: function() {
+            this.setElement(this.parse());
+            this.is_created = true;
+            return this;
+        },
+
+        /** 
+         * Update this view.  Don't recreate it, instead insert the new data
+         * into the existing element.
+         *
+         * @return  {void}
+         */
+        update: function() {
+            this.$el.find("a.title")
+                .html(this.model.get('genus') + ' ' + this.model.get('species')); 
+            this.$el.find("span.sub-title")
+                .html(this.model.get('common_names')[0].name);
+            return this;
+
+        },
+
+        /**
+         * Render this plant display box from the template.
+         *
+         * @return  {void}
+         */
+		render: function() {
+            if( ! this.is_created) {
+                this.create();
+            } else {
+                this.update();
+            }
+
 			return this;
 		}
 	});
+
+    return PlantBoxView;
 });
