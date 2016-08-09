da978ef20eac2f3f2d08bc7b74010f0b97d6ba9e
Daniel Bingham <dbingham@theroadgoeson.com>
2016-08-08 22:05:09 -0400

    Playing with the design.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,25 +3,29 @@
 <head>
 	<title>Forest to Farm</title>
 	<link rel="stylesheet" type="text/css" href="/styles/forest.css" />
-	<script src="/scripts/library/require.js" data-main="/scripts/bootstrap"></script>
+    <script src="/scripts/library/require.js" data-main="/scripts/bootstrap"></script>
+	<link href="https://fonts.googleapis.com/css?family=Arima+Madurai|Cabin+Sketch" rel="stylesheet">
 </head>
 <body>
-<div id="header">
-    <div id="logo"><a href="/">Forest to Farm</a></div>
-	<div class="clear"></div>
-</div>
+<header>
+	<nav>
+		<a href="" class="icon-search"></a>
+		<a href="" class="icon-menu"></a>
+	</nav>
+    <h1><a href="/"><p id="forest">Forest</p> <p>to</p> <p id="farm">Farm</p></a></h1>
+	<h2>A tool for permaculture and restoration agriculture.</h2>
+</header>
 <div id="main">
 	<ul id="plants">
-
-
 	</ul>
 </div>
-<div id="footer" >
+<footer>
 	<div class="attributions">
 		<span id="tree-image-attribution">Tree Image from: Dieter Tracey,
 		Integration and Application Network, University of Maryland Center for
 		Environmental Science (ian.umces.edu/imagelibrary/).</span>
 	</div>
+</footer>
 </body>
 </html>
 
