654bbab7b2b6a83a6bf997195d1e0c775bd62dc6
Daniel Bingham <dbingham@theroadgoeson.com>
2016-08-10 17:50:20 -0400

    Tweaking the header design and the color scheme.

--- public/scripts/app/main.js
+++ public/scripts/app/main.js
@@ -5,4 +5,30 @@ require([
     'router'],
 function($, _, Backbone, Router) {
     var router = new Router();
+
+    $("header nav a.icon-menu").click(function(e) {
+        e.preventDefault();     
+        $("header nav a.icon-menu").hide();
+        $("nav#main-menu").animate({width: "toggle", opacity: "toggle"}, 200);
+        return false;
+    });
+
+    var hideMenu = function() {
+        $("nav#main-menu").animate(
+            {width: "toggle", opacity: "toggle"}, 
+            200, 
+            function() {
+                $("header nav a.icon-menu").show();
+            }
+        );
+    };
+
+    $("nav#main-menu a").click(hideMenu);
+    $("nav#main-menu a.icon-menu").click(function(e) {
+        e.preventDefault();
+        return false;
+    });
+
+   
+   
 });
