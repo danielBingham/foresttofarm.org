4004922fc0240b92623394c60c3a2e593ac8e53b
Daniel Bingham <dbingham@theroadgoeson.com>
2016-08-19 13:56:10 -0400

    Running NPM install as part of vagrants bootup.

--- cookbooks/foresttofarm-development/recipes/grunt.rb
+++ cookbooks/foresttofarm-development/recipes/grunt.rb
@@ -6,3 +6,10 @@
 
 # Install grunt client
 nodejs_npm "grunt-cli"
+
+# We need to run npm install in order to install all of grunt's dependencies.
+# Should pick up in project.json with out any help.
+execute "Run 'npm install' in the project directory" do
+    cwd node['foresttofarm.org']['source_directory']
+    command "npm install"
+end
