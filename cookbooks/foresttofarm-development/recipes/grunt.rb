0eced80458108c91d4721ad503ac31de369a1e41
Daniel Bingham <dbingham@theroadgoeson.com>
2016-08-24 19:52:12 -0400

    Adding vagrant user to the www-data group so we can run grunt.

--- cookbooks/foresttofarm-development/recipes/grunt.rb
+++ cookbooks/foresttofarm-development/recipes/grunt.rb
@@ -13,3 +13,11 @@ execute "Run 'npm install' in the project directory" do
     cwd node['foresttofarm.org']['source_directory']
     command "npm install"
 end
+
+# Append the vagrant user to the www-data group.  We need to do this
+# because grunt needs write acccess to the source_directory.
+group 'www-data' do
+    action :modify
+    members 'vagrant'
+    append  true
+end
