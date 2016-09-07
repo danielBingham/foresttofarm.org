2a8d4c8ff73d4c229634b7bd1c74741c2b8f4997
Daniel Bingham <dbingham@theroadgoeson.com>
2016-09-07 14:10:32 -0400

    More rubyish spacing for PlantsController.

--- app/controllers/api/v0/plants_controller.rb
+++ app/controllers/api/v0/plants_controller.rb
@@ -1,26 +1,26 @@
 class Api::V0::PlantsController < ApplicationController
 
-    # Retrieve a json list of all plants in the database.
-    def index
-      @plants = Plant.includes(:common_names).all
+  # Retrieve a json list of all plants in the database.
+  def index
+    @plants = Plant.includes(:common_names).all
 
-        render :json => @plants, :include => { :common_names =>{} }
-    end
+    render :json => @plants, :include => { :common_names =>{} }
+  end
 
-    # Retrieve details of a single plant in the database.
-    #
-    # * *params*:: 
-    #   - +id+ -> The id of the plant to show.
-    # * *returns*:: nil
-    def show
-      @plant = Plant.includes(
-        :common_names, :harvests, :drawbacks, :habits, 
-        :habitats, :light_tolerances, :moisture_tolerances, 
-        :roles, :root_patterns).find(params[:id])
+  # Retrieve details of a single plant in the database.
+  #
+  # * *params*:: 
+  #   - +id+ -> The id of the plant to show.
+  # * *returns*:: nil
+  def show
+    @plant = Plant.includes(
+      :common_names, :harvests, :drawbacks, :habits, 
+      :habitats, :light_tolerances, :moisture_tolerances, 
+      :roles, :root_patterns).find(params[:id])
 
-      render :json => @plant, :include => [
-        :common_names, :harvests, :drawbacks, :habits, 
-        :habitats, :light_tolerances, :moisture_tolerances, 
-        :roles, :root_patterns]
-    end
+    render :json => @plant, :include => [
+      :common_names, :harvests, :drawbacks, :habits, 
+      :habitats, :light_tolerances, :moisture_tolerances, 
+      :roles, :root_patterns]
+  end
 end
