a8abfe60d6e9372e1d962b1b3aba91348764bbb1
Daniel Bingham <dbingham@theroadgoeson.com>
2016-09-07 14:27:57 -0400

    Removing extraneous SiteController and added appropriate actions to Site::PlantsController.

--- app/controllers/site/plants_controller.rb
+++ app/controllers/site/plants_controller.rb
@@ -1,3 +1,9 @@
 class Site::PlantsController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
 
 end
--- app/controllers/site_controller.rb
+++
@@ -1,9 +0,0 @@
-class Site::SiteController < ApplicationController
-
-  def index
-  end
-
-  def show
-  end
-
-end
