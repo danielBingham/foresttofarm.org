76f2330dd0f7e5326d6746ca02d5690bf7a630a7
Daniel Bingham <dbingham@theroadgoeson.com>
2016-09-09 11:51:40 -0400

    Removing mini test files.

--- test/controllers/api/v0/plants_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::V0::PlantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/common_name_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CommonNameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
